f05939d4b71d35dea333b3a79c3d193c37956015
ttraintimes <ttraintimes@gmail.com>
2024-05-07 15:00:14 +0200

    1

---
+++ brainsound/Assets/AutoEscape.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AutoEscape : MonoBehaviour
+{
+    public float timeoutDuration = 30f; // Timeout duration in seconds
+    private float elapsedTime = 0f;
+    private bool playerInputDetected = false;
+    public GameObject escMenu;
+    public UnityEvent onMenuAppear;
+    public UnityEvent onMenuDisppear;
+
+    void Update()
+    {
+        // Check if any player input is detected
+        if (Input.anyKey || Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            playerInputDetected = true;
+        }
+
+        // Increment elapsed time if no player input detected
+        if (!playerInputDetected)
+        {
+            elapsedTime += Time.deltaTime;
+
+            // If timeout duration reached, simulate ESC button press
+            if (elapsedTime >= timeoutDuration)
+            {
+                // Call a function to simulate ESC button press
+                 escMenu.SetActive(false);
+            Time.timeScale = 1;
+
+            onMenuDisppear.Invoke();
+            }
+             else
+        {
+            escMenu.SetActive(true);
+            Time.timeScale = 0;
+            onMenuAppear.Invoke();
+        }
+        }
+    }
+    
+}
---
+++ brainsound/Assets/boardUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class boardUI : MonoBehaviour
+{
+    GameObject raycastedObj;
+    ObjectInteraction referencedScript;
+    public GameObject boardScene;
+    public UnityEvent onMenuAppear;
+
+    [SerializeField] private int InteractionRange = 2;
+    [SerializeField] private LayerMask scenetransitionLayer;
+
+    // Update is called once per frame
+
+
+    void Start()
+    {
+        boardScene.SetActive(false);
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, out hit, InteractionRange, scenetransitionLayer.value))
+        {
+            if (hit.collider.CompareTag("board"))
+            {
+                boardScene.SetActive(true);
+                onMenuAppear.Invoke();
+                raycastedObj = hit.collider.gameObject;
+                referencedScript = raycastedObj.GetComponent<ObjectInteraction>();
+            }
+        }
+        else
+        {
+            boardScene.SetActive(false);
+        }
+
+    }
+}
